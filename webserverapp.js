1110622bc21d6c8d3daec95bc23262b88ec224d4
Mohamed Alhajri <89851240+MohHajri@users.noreply.github.com>
2022-09-26 11:21:24 +0300

    Update webserverapp.js

--- webserverapp.js
+++ webserverapp.js
@@ -1,10 +1,10 @@
 const http = require("http");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const hostname = "127.0.0.1";
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World it is me!");
+  res.end("Hello World it is Hajri!");
 });
 
 server.listen(port, hostname, () => {
